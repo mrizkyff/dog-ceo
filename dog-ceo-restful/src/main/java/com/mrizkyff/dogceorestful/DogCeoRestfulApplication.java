bd6121a0252b39cc1f9c873552e3d9bda2ccf56e
mrizkyff <muhamadrizkyff@gmail.com>
2024-03-15 14:56:53 +0700

    disable caching

--- dog-ceo-restful/src/main/java/com/mrizkyff/dogceorestful/DogCeoRestfulApplication.java
+++ dog-ceo-restful/src/main/java/com/mrizkyff/dogceorestful/DogCeoRestfulApplication.java
@@ -2,12 +2,10 @@ package com.mrizkyff.dogceorestful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableCaching
 public class DogCeoRestfulApplication {
     public static void main(String[] args) {
         SpringApplication.run(DogCeoRestfulApplication.class , args);
