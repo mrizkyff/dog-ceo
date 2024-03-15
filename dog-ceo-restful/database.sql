952e11b63b92b1908183239e4f5a360e0e6d2a6a
mrizkyff <muhamadrizkyff@gmail.com>
2024-03-15 15:21:14 +0700

    ddl

--- dog-ceo-restful/database.sql
+++ dog-ceo-restful/database.sql
@@ -1,24 +1,47 @@
-DROP TABLE IF EXISTS sub_breeds;
-DROP TABLE IF EXISTS breeds;
-
-CREATE TABLE IF NOT EXISTS breeds (
-                        id uuid not null,
-                        created_date timestamp(6) with time zone,
-                        last_modified_date timestamp(6) with time zone,
-                        name varchar(255) not null unique ,
-                        primary key (id)
+create table breeds
+(
+    id                 uuid         not null,
+    created_date       timestamp(6) with time zone,
+    last_modified_date timestamp(6) with time zone,
+    name               varchar(255) not null,
+    ttl                bigint,
+    primary key (id)
 );
 
-CREATE TABLE IF NOT EXISTS sub_breeds (
-                            id uuid not null,
-                            created_date timestamp(6) with time zone,
-                            last_modified_date timestamp(6) with time zone,
-                            name varchar(255) not null unique ,
-                            breed_id uuid,
-                            primary key (id)
+
+create table sub_breeds
+(
+    id                 uuid         not null,
+    created_date       timestamp(6) with time zone,
+    last_modified_date timestamp(6) with time zone,
+    name               varchar(255) not null,
+    breed_id           uuid         not null,
+    primary key (id)
 );
+
+
+create table sub_breed_images
+(
+    sub_breed_id uuid not null,
+    images       varchar(255)
+);
+
+
+alter table if exists breeds
+    drop constraint if exists UK_a3bphmgjsvn8aleiujdw0sbef;
+
+
+alter table if exists breeds
+    add constraint UK_a3bphmgjsvn8aleiujdw0sbef unique (name);
+
 
 alter table if exists sub_breeds
     add constraint FKkil4eq9saoo5k2us1xwiqj1k6
         foreign key (breed_id)
             references breeds;
+
+
+alter table if exists sub_breed_images
+    add constraint FK3jsyfjyvv2oilocjfr3edqmhd
+        foreign key (sub_breed_id)
+            references sub_breeds;
